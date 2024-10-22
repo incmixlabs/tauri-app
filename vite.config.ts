6a90a234a123d75429116e1a4f6163eaf6d453cb
uma-uc <uma.apple123@gmail.com>
2024-10-21 23:52:45 -0700

    chore: tauri-ios

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { internalIpV4 } from 'internal-ip'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -15,14 +16,14 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    port: 5173,
     strictPort: true,
-    host: host || false,
+    host: "0.0.0.0",
     hmr: host
       ? {
           protocol: "ws",
-          host,
-          port: 1421,
+          host: await internalIpV4(),
+          port: 5183,
         }
       : undefined,
     watch: {
@@ -30,6 +31,7 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
+  envPrefix: ["VITE_", "TAURI_"],
   resolve: {
        alias: {
        "~": path.resolve(__dirname, "./src"),
